464523c4b9d34d275869931575185a9d5ecb95d6
OllgaDockerr <ofikss113@gmail.com>
2025-05-27 23:44:37 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+#!/usr/bin/env bash
+set -o errexit
+
+echo "===> START.SH IS RUNNING <==="
+
+python tabbycat/run-asgi.py
+
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 gunicorn tabbycat.wsgi:application --bind 0.0.0.0:$PORT
