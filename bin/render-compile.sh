0bbe93e0610c9dd87641ed58ff213e9d971c3a3d
OllgaDockerr <ofikss113@gmail.com>
2025-05-27 23:36:20 +0500

    Update render-compile.sh

--- bin/render-compile.sh
+++ bin/render-compile.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-chmod +x ./start.sh
-
 # exit on error
 set -o errexit
-
+chmod +x ./start.sh
 echo "-----> Install dependencies"
 python -m pip install pipenv
 pipenv install --system
