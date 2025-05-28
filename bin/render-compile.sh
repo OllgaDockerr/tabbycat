7abeeed0ade59218eed011c0b00300614986aa63
OllgaDockerr <ofikss113@gmail.com>
2025-05-28 18:21:29 +0500

    Update render-compile.sh

--- bin/render-compile.sh
+++ bin/render-compile.sh
@@ -3,8 +3,6 @@
 set -o errexit
 chmod +x ./start.sh
 echo "-----> Install dependencies"
-python -m pip install pipenv
-pipenv install --system
 pip install -r requirements.txt && python manage.py collectstatic --noinput
 
 echo "-----> I'm post-compile hook"
